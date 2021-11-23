eb6d8d4128081ce587c84ce9f3f82fb1db79b867
Paul Albandoz <palbandoz@gmail.com>
2021-11-24 00:12:15 +0100

    sports header

--- resources/views/sports/create.blade.php
+++ resources/views/sports/create.blade.php
@@ -1,15 +1,7 @@
 @extends('layouts.main')
 
 @section('main-board')
-    <h1 class="bg-gray-500 text-gray-100 border-b-4 border-yellow-400 mb-2 flex items-center space-x-8">
-        <a role="button" href="{{ route('sports.index') }}" class="p-2 hover:bg-gray-400">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10h10a8 8 0 018 8v2M3 10l6 6m-6-6l6-6" />
-            </svg>
-        </a>
-
-        <span class="font-bold">@lang('sport.button.new')</span>
-    </h1>
+    @include('sports.parts.header')
 
     <form action='{{ route('sports.store') }}' method='post'>
         @csrf
--- resources/views/sports/show.blade.php
+++ resources/views/sports/show.blade.php
@@ -1,15 +1,7 @@
 @extends('layouts.main')
 
 @section('main-board')
-    <h1 class="bg-gray-500 text-gray-100 border-b-4 border-yellow-400 mb-2 flex items-center space-x-8">
-        <a role="button" href="{{ route('sports.index') }}" class="p-2 hover:bg-gray-400">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10h10a8 8 0 018 8v2M3 10l6 6m-6-6l6-6" />
-            </svg>
-        </a>
-
-        <span class="font-bold">{{ $sport->name }}</span>
-    </h1>
+    @include('sports.parts.header')
 
     <div class="flex space-x-1">
         <a role="button" href="{{ route('sports.edit', $sport->id) }}" class="inline-block border-solid border-4 border-purple-200 text-purple-600 py-2 px-4 rounded-full text-sm font-bold uppercase tracking-wider hover:bg-purple-200">
